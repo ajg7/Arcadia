c16a8826df5a6964300606bfc10ddb187a4da63f
AJ Gebara <ajgebara@gmail.com>
2023-02-09 00:07:26 -0600

    added simple guide to start making MVP

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,10 +1,9 @@
 import React from "react";
+import Odyssey from "./Odyssey.tsx";
 
 const App = (): JSX.Element => {
   return (
-    <div>
-      <h1>Hello World!!</h1>
-    </div>
+    <Odyssey />
   );
 };
 
---
+++ src/containers/Odyssey.tsx/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Odyssey = (): JSX.Element => {
+  return (
+    <div>
+      <h1>Book</h1>
+      <h2>Adventure 1</h2>
+      <h2>Adventure 2</h2>
+      <h2>Adventure 3</h2>
+      <h3>Battle!</h3>
+    </div>
+  );
+};
+
+export default Odyssey;
