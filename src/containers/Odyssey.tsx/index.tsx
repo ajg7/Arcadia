99413df59b2635984df4b36d3d26b5fe95671bc0
AJ Gebara <ajgebara@gmail.com>
2023-03-03 14:46:15 -0600

    can now add adventure titles

--- src/containers/Odyssey.tsx/index.tsx
+++ src/containers/Odyssey.tsx/index.tsx
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 const Odyssey = (): JSX.Element => {
   /*
     Endgoal: We need to create a system where you can create an Odyssey -> Adventures -> Battle
-    1. Add an Odyssey
+    1. Add adventures
   */
 
   const [odysseyTitle, setOdysseyTitle] = useState<string>("");
@@ -11,12 +11,26 @@ const Odyssey = (): JSX.Element => {
     useState<boolean>(false);
   const [isButtonActive, setIsButtonActive] = useState<boolean>(true);
   const [isFormSubmitted, setIsFormSubmitted] = useState<boolean>(false);
+  const [isAdventureCreateActive, setIsAdventureCreateActive] =
+    useState<boolean>(false);
+  const [adventures, setAdventures] = useState<Array<string>>([]);
+  const [adventureTitle, setAdventureTitle] = useState<string>("");
+  const [isAdventureTextBoxActive, setIsAdventureTextBoxActive] =
+    useState<boolean>(false);
 
   const submitHandler = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     setIsOdysseyTextBoxActive(false);
     setIsButtonActive(false);
     setIsFormSubmitted(true);
+    setIsAdventureCreateActive(true);
+  };
+
+  const submitAdventureHandler = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setIsAdventureCreateActive(true);
+    setIsAdventureTextBoxActive(false);
+    setAdventures((prevState) => [...prevState, adventureTitle]);
   };
 
   return (
@@ -33,10 +47,32 @@ const Odyssey = (): JSX.Element => {
         <button
           onClick={() => setIsOdysseyTextBoxActive(!isOdysseyTextBoxActive)}
         >
-          {"Add an Odyssey"}
+          {"Add an Odyssey!"}
         </button>
       )}
       {isFormSubmitted && <h1>{odysseyTitle}</h1>}
+      {isAdventureTextBoxActive && (
+        <form onSubmit={submitAdventureHandler}>
+          <input
+            type="text"
+            onChange={({ target: { value } }) => setAdventureTitle(value)}
+          />
+        </form>
+      )}
+      {isAdventureCreateActive && (
+        <button
+          onClick={() => setIsAdventureTextBoxActive(!isAdventureTextBoxActive)}
+        >
+          {"Add an Adventure!"}
+        </button>
+      )}
+      {adventures.map((adventure) => {
+        return (
+          <div>
+            <h3>{adventure}</h3>
+          </div>
+        );
+      })}
     </div>
   );
 };
