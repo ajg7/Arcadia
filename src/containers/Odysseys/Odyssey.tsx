d6554fa8235fc7a752c5c2b977b98cbd0e5ae9af
AJ Gebara <ajgebara@gmail.com>
2023-03-12 19:10:37 -0500

    moved the create button to the top of the page

--- src/containers/Odysseys/Odyssey.tsx
+++ src/containers/Odysseys/Odyssey.tsx
@@ -2,12 +2,18 @@ import React from "react";
 import { Adventures } from "../../components/adventures/index";
 import Title from "../../components/title/title";
 import { OdysseyHeader } from "./styles";
+import CreateButton from "../../components/create-button/CreateButton";
 
 const Odyssey = () => {
   return (
-    <OdysseyHeader>
-      <Title title={"Hegelian Dialectics"} />
-    </OdysseyHeader>
+    <>
+      <div>
+        <CreateButton />
+      </div>
+      <OdysseyHeader>
+        <Title title={"Hegelian Dialectics"} />
+      </OdysseyHeader>
+    </>
   );
 };
 
